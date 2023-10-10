560260ae7934f41b1d63814e127e07261c1177ab
aheed <aheedkhan03@gmail.com>
2023-10-10 19:06:47 +0500

    full complete

--- vite-air-bnb-clone-project/src/components/Section1.jsx
+++ vite-air-bnb-clone-project/src/components/Section1.jsx
@@ -7,8 +7,9 @@ export default function Section1() {
       <img src={Grid} className="section1--image" />
       <h1 className="section1--heading">Online Experiences</h1>
       <p className="section1--para">
-        Join unique interactive activities led by one-of-a-kind hosts—all
-        without leaving home.
+        Join unique interactive activities led by
+        <br /> one-of-a-kind hosts—all without leaving <br />
+        home.
       </p>
     </section>
   );
