731f199e5b563703a4b71454f161c1fadf26ef43
aheed <aheedkhan03@gmail.com>
2023-10-11 13:48:07 +0500

    designing

--- vite-meme_generator/src/App.jsx
+++ vite-meme_generator/src/App.jsx
@@ -1,29 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "raect";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>Meme Generator is Under Development :D</p>
-      </div>
-      <p>By Aheed Tahir {" <3 "}</p>
+      <Header />
+      <Meme />
     </>
   );
 }
