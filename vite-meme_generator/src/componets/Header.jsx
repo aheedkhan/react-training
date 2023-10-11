b72467a4f6157f00eb006b6903c6744107362e4d
aheed <aheedkhan03@gmail.com>
2023-10-11 16:01:33 +0500

    started working on project

--- vite-meme_generator/src/componets/Header.jsx
+++ vite-meme_generator/src/componets/Header.jsx
@@ -4,9 +4,9 @@ import MemeLogo from "/public/images/troll-face.png";
 export default function Header() {
   return (
     <nav>
-      <img src={MemeLogo} />
-      <h2>Meme Generator</h2>
-      <h4>React Course - Project 3</h4>
+      <img className="nav--image" src={MemeLogo} />
+      <h2 className="nav--title">Meme Generator</h2>
+      <h4 className="nav--content">React Course - Project 3</h4>
     </nav>
   );
 }
--- vite-meme_generator/src/componets/Meme.jsx
+++ vite-meme_generator/src/componets/Meme.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Meme() {
-  return <main>testing</main>;
+  return (
+    <main>
+      <input
+        className="input--box"
+        type="text"
+        placeholder="Upper Text"
+      ></input>
+      <input
+        className="input--box"
+        type="text"
+        placeholder="Lower Text"
+      ></input>
+      <button className="meme-btn">Get a new meme image 🖼</button>
+    </main>
+  );
 }
