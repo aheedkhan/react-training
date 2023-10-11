df7a1789b42d04c685b77cf6e61a1886f164852e
aheed <aheedkhan03@gmail.com>
2023-10-12 03:25:08 +0500

    random memes added

--- vite-meme_generator/src/componets/Meme.jsx
+++ vite-meme_generator/src/componets/Meme.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import memesData from "/src/memesData";
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState("https://i.imgflip.com/1ur9b0.jpg");
+
+  function getMemeUrl() {
+    const memeArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    setMeme((prev) => (prev = memeArray[randomNumber].url));
+  }
+
   return (
     <main className="form">
       <input
@@ -13,7 +22,10 @@ export default function Meme() {
         type="text"
         placeholder="Lower Text"
       ></input>
-      <button className="form--button">Get a new meme image 🖼</button>
+      <button onClick={getMemeUrl} className="form--button">
+        Get a new meme image 🖼
+      </button>
+      <img className="meme--image" src={meme} />
     </main>
   );
 }
