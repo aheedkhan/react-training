f91ff98e9b153566ebca032aaac38e326da19249
aheed <aheedkhan03@gmail.com>
2023-10-18 01:35:27 +0500

    css added and statechange added

--- vite-meme_generator/src/componets/Header.jsx
+++ vite-meme_generator/src/componets/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import MemeLogo from "/public/images/troll-face.png";
 
 export default function Header() {
   return (
-    <nav>
-      <img className="nav--image" src={MemeLogo} />
-      <h2 className="nav--title">Meme Generator</h2>
-      <h4 className="nav--content">React Course - Project 3</h4>
-    </nav>
+    <header className="header">
+      <img src="./images/troll-face.png" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
   );
 }
